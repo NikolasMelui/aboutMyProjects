8217353aa37df44c8256bfb33a7bc2b4857434d8
NikolasMelui <slak@samaradom.ru>
2018-01-27 00:06:18 +0400

    Add model stringify.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import Link from 'gatsby-link';
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
 	<div>
 		<h1>About all projects done by NikolasMelui.</h1>
 		<p>Today we're going to use some new techs like graphQL and Contentful.</p>
 		<Link to="/page-2/">Go to page 2</Link>
+		<p>{JSON.stringify(data)}</p>
 	</div>
 );
 
 export default IndexPage;
+
+export const query = graphql`
+	query PageQuery {
+		allContentfulBrand {
+			edges {
+				node {
+					product {
+						id
+					}
+				}
+			}
+		}
+	}
+`;
