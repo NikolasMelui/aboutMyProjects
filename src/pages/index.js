0cb700f9e7058caba407e6178aba42148ef7e115
NikolasMelui <slak@samaradom.ru>
2018-01-26 22:44:55 +0400

    Add new information.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,12 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from 'react';
+import Link from 'gatsby-link';
 
 const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-)
+	<div>
+		<h1>About all projects done by NikolasMelui.</h1>
+		<p>Today we're going to use some new techs like graphQL and Contentful.</p>
+		<Link to="/page-2/">Go to page 2</Link>
+	</div>
+);
 
-export default IndexPage
+export default IndexPage;
