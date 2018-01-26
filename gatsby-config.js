a6dfd43c7de3b85694f98eef4ec655ed56b6238e
NikolasMelui <slak@samaradom.ru>
2018-01-26 23:16:57 +0400

    Add access keys and some new info to config.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,15 @@
 module.exports = {
-  siteMetadata: {
-    title: 'Gatsby Default Starter',
-  },
-  plugins: ['gatsby-plugin-react-helmet'],
+	siteMetadata: {
+		title: 'Gatsby Default Starter',
+	},
+	plugins: [
+		'gatsby-plugin-react-helmet',
+		{
+			resolve: `gatsby-source-contentful`,
+			options: {
+				spaceId: `dzegfqba74y6`,
+				accessToken: `39f68a1dfab8bfb9b26b29ea6d664fca165d8bef9ad6d5834a76fa3f8eb5de79`,
+			},
+		},
+	],
 };
